6f5a41c1f3a65ad7e4112da617e8df183b6cf09e
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-12 11:47:38 -0300

    creating tabs

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
 
-export default props => (
-    <div>
-        <h1>Ciclo de Pagamento</h1>
-    </div>
-)
+ 
+
+import React, { Component } from 'react'
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+import Tabs from '../common/tab/tabs'
+import TabsContent from '../common/tab/tabsContent'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabHeader from '../common/tab/tabHeader'
+
+class BillingCycle extends Component{
+    render(){
+        return(
+            <div>
+                <ContentHeader title='Ciclo de Pagamentos' small='Cadastro'>
+                </ContentHeader>
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList'></TabHeader>
+                            <TabHeader label='Inluir' icon='plus' target='tabCreate'></TabHeader>
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate'></TabHeader>
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete'></TabHeader>
+
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default BillingCycle
