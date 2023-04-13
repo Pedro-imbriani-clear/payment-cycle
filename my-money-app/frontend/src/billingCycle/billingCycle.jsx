f826fe264da0ca81dfc49ca7be1181b5a8122e4e
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-13 11:14:10 -0300

    form with redux and error/success messages

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -10,7 +10,9 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab,showTabs } from '../common/tab/tabActions'
+import { create } from './billingCycleActions'
 import List from './billingCycleList'
+import Form from './billingCycleForm'
 
 class BillingCycle extends Component{
     componentWillMount(){
@@ -24,26 +26,32 @@ class BillingCycle extends Component{
                 </ContentHeader>
                 <Content>
                     <Tabs>
+
                         <TabsHeader>
                             <TabHeader label='Listar' icon='bars' target='tabList'></TabHeader>
                             <TabHeader label='Inluir' icon='plus' target='tabCreate'></TabHeader>
                             <TabHeader label='Alterar' icon='pencil' target='tabUpdate'></TabHeader>
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete'></TabHeader>
-
                         </TabsHeader>
+
                         <TabsContent>
                             <TabContent id='tabList'>
                                 <List/>
                             </TabContent>
-                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabCreate'>
+                                <Form onSubmit={this.props.create}/>
+                            </TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
+
                     </Tabs>
                 </Content>
             </div>
         )
     }
 }
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab,showTabs},dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({
+    selectTab,showTabs,create
+},dispatch)
 export default connect(null,mapDispatchToProps)(BillingCycle)
