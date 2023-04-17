e39e5ca56dd87ad5efdb537cafff3ab0a002a881
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-04-17 12:55:01 -0300

    changes to new version

--- my-money-app/frontend-new/src/main/routes.jsx
+++ my-money-app/frontend-new/src/main/routes.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { Router, Route, Redirect, hashHistory,IndexRoute } from 'react-router'
+import { Switch, Route, Redirect, } from 'react-router'
 
 import AuthOrApp from "./authOrApp";
 import Dashboard from "../dashboard/dashboard";
 import BillingCycle from "../billingCycle/billingCycle";
 
 export default props=>(
-    <Router history={hashHistory}>
-        <Route path='/' component={AuthOrApp}>
-            <IndexRoute component={Dashboard}/>
-        <Route path='billingCycles' component={BillingCycle}></Route>
-        </Route>  
+  <div className="content-wrapper">
+    <Switch>
+        <Route exact path='/' component={Dashboard}/>
+        <Route path='/billingCycle' component={BillingCycle}/>
         <Redirect from='*' to='/'/>
-
-    </Router>
+    </Switch>
+  </div>
 )
